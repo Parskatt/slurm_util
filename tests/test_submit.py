6e7378d95499d337e6b2b903836707ea1c9e6c71
Johan Edstedt <johan.edstedt@liu.se>
2025-08-25 18:58:48 +0200

    prep alvis

--- tests/test_submit.py
+++ tests/test_submit.py
@@ -1,8 +1,9 @@
 import subprocess
 
 def test_submit():
-    subprocess.run(["submit", "-g", "1", "--dry_run"], check=True)
-    subprocess.run(["submit", "-g", "1", "--dry_run", "--interactive"], check=True)
+    subprocess.run(["submit", "-g", "1", "-t", "0-00:01:00", "tests/test_job.py"], check=True)
+    subprocess.run(["submit", "-g", "1", "-t", "0-00:01:00", "tests/test_job.py"], check=True)
+    subprocess.run(["submit", "-g", "1", "-t", "0-00:01:00", "-i"], check=True)
 
 
 if __name__ == "__main__":
